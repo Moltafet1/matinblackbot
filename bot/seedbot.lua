3bce82071723a8429b37b7db36ca4491a335511e
Moltafet1 <matin.black.75@gmail.com>
2016-01-14 14:18:13 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {104091819,87917979,0,tonumber(our_id)},--Sudo users
+    sudo_users = {149223247,159769161,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
